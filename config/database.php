0843d0166c0b95bb5ae23501ab35388418730ab5
Irfan Syafiq Azmy Aziz <irf28004@gmail.com>
2026-06-22 15:50:10 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,22 +1,27 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "iphone_database";
+header("Content-Type: application/json");
+
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
 $conn = new mysqli(
     $host,
     $user,
     $password,
-    $database
+    $database,
+    $port
 );
 
 if ($conn->connect_error) {
     die(
         json_encode([
             "success" => false,
-            "message" => "Database connection failed"
+            "message" => "Database connection failed",
+            "error" => $conn->connect_error
         ])
     );
 }
